1d18244293f7ec54690bd9f48570bbc8ba96a9bb
mrafiq709 <mrafiq709@gmail.com>
2019-10-08 00:14:38 +0700

    onload

--- event.js
+++ event.js
@@ -21,6 +21,18 @@ function onMouseOut(){
 	elemId('mouseOver').style.backgroundColor = "lightblue";
 }
 
+var cnt = 0;
+// onkeydown
+function onKeyPress(){
+	elemId('keyPress').innerHTML = "keyPress" + cnt;
+	cnt++;
+}
+
+// onload
+function myOnLoad(){
+	alert('Page Loaded !');
+}
+
 // querySelector
 function $(elem) {
   return document.querySelector(elem);
